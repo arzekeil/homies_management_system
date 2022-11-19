45f84a55ee9daaa3accdcc6ba682bb46e3829230
Arzekeil-Abel De Leon <delearze@gmail.com>
2022-11-18 23:52:01 -0500

    added icons to navigation tabs

--- src/Tabs/Tabs.js
+++ src/Tabs/Tabs.js
@@ -1,4 +1,5 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { FontAwesome } from '@expo/vector-icons';
 
 import CostsScreen from "../Splitwise/Splitwise-Tab";
 import TodoScreen from "../Todo/Todo-Tab";
@@ -7,7 +8,24 @@ const Tab = createBottomTabNavigator();
 
 function BottomTabNavigation() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+    screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Costs') {
+            iconName = 'dollar';
+          } else if (route.name === 'Todo') {
+            iconName = 'list';
+          }
+
+          // You can return any component that you like here!
+          return <FontAwesome name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: 'tomato',
+        tabBarInactiveTintColor: 'gray',
+      })}
+    >
       <Tab.Screen name="Costs" component={CostsScreen} />
       <Tab.Screen name="Todo" component={TodoScreen} />
     </Tab.Navigator>
