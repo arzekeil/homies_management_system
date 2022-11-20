d32182fd3c3a00d4ea2bc3928c6bf39cce5117eb
Arzekeil-Abel De Leon <delearze@gmail.com>
2022-11-20 05:00:55 -0500

    Uses fab instaed of icon buttons

--- client/lib/src/post_form_page.dart
+++ client/lib/src/post_form_page.dart
@@ -12,109 +12,77 @@ class _PostFormState extends State<PostForm> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('New Post!'),
+        title: const Text('New Post'),
         backgroundColor: Colors.deepOrange[300],
       ),
       body: Column(
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Material(
-                child: Center(
-                  child: Ink(
-                    child: IconButton(
-                      icon: const Icon(Icons.close),
-                      color: Colors.black,
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
-                    ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+            child: TextField(
+              maxLines: null,
+              cursorColor: Colors.grey,
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                hintStyle: const TextStyle(color: Colors.grey),
+                labelStyle: const TextStyle(color: Colors.grey),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                  borderSide: const BorderSide(
+                    width: 1,
+                    color: Colors.black,
                   ),
                 ),
-              ),
-              Material(
-                child: Center(
-                  child: Ink(
-                    child: IconButton(
-                      icon: const Icon(Icons.send),
-                      color: Colors.black,
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
-                    ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                  borderSide: const BorderSide(
+                    width: 1,
+                    color: Colors.black,
                   ),
                 ),
+                labelText: 'Post Title',
               ),
-            ],
+              style: const TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
+            ),
           ),
-          Column(
-            children: [
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-                child: TextField(
-                  maxLines: null,
-                  cursorColor: Colors.grey,
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.white,
-                    hintStyle: const TextStyle(color: Colors.grey),
-                    labelStyle: const TextStyle(color: Colors.grey),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15.0),
-                      borderSide: const BorderSide(
-                        width: 1,
-                        color: Colors.black,
-                      ),
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15.0),
-                      borderSide: const BorderSide(
-                        width: 1,
-                        color: Colors.black,
-                      ),
-                    ),
-                    labelText: 'Post Title',
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+            child: TextField(
+              maxLines: null,
+              cursorColor: Colors.grey,
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                hintStyle: const TextStyle(color: Colors.grey),
+                labelStyle: const TextStyle(color: Colors.grey),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                  borderSide: const BorderSide(
+                    width: 1,
+                    color: Colors.black,
                   ),
-                  style: const TextStyle(
-                      fontSize: 24, fontWeight: FontWeight.w600),
                 ),
-              ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-                child: TextField(
-                  maxLines: null,
-                  cursorColor: Colors.grey,
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.white,
-                    hintStyle: const TextStyle(color: Colors.grey),
-                    labelStyle: const TextStyle(color: Colors.grey),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15.0),
-                      borderSide: const BorderSide(
-                        width: 1,
-                        color: Colors.black,
-                      ),
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15.0),
-                      borderSide: const BorderSide(
-                        width: 1,
-                        color: Colors.black,
-                      ),
-                    ),
-                    labelText: 'Post Description',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                  borderSide: const BorderSide(
+                    width: 1,
+                    color: Colors.black,
                   ),
-                  style: const TextStyle(
-                      fontSize: 16, fontWeight: FontWeight.w400),
                 ),
+                labelText: 'Post Description',
               ),
-            ],
+              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+            ),
           ),
         ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).pop();
+        },
+        backgroundColor: Colors.deepOrange.shade200,
+        child: const Icon(Icons.send),
       ),
     );
   }
