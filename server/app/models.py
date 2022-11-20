aa60d2987a8bf3e14b4c29708c6da9d2c05f516e
Angela Wang <74735037+angelaw7@users.noreply.github.com>
2022-11-19 20:09:20 -0500

    Arzq backend yay (#9)
    * :D
    
    * :D (#6)
    
    * refactor frontend into client directory
    
    * forgot about poor package-lock.json
    
    * good changes
    
    * :DDD
    
    * merge arzq onto main
    
    Co-authored-by: Arzekeil-Abel De Leon <delearze@gmail.com>
    Co-authored-by: Arzekeil-Abel De Leon <56529126+arzekeil@users.noreply.github.com>

---
+++ server/app/models.py
@@ -0,0 +1,53 @@
+from sqlalchemy import Boolean, Column, Integer, String, Date, ForeignKey
+# from sqlalchemy.orm import relationship
+
+from .database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    firstName = Column(String, index=True)
+    lastName = Column(String, index=True)
+    icon = Column(String, index=True)
+    color = Column(String, index=True)
+
+
+class Todo(Base):
+    __tablename__ = "todos"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    dueDate = Column(Date, index=True)
+    userId = Column(String, index=True)
+    isCompleted = Column(Boolean, index=True)
+
+
+class Post(Base):
+    __tablename__ = "posts"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    date = Column(Date, index=True)
+    userId = Column(Integer, index=True)
+    description = Column(String, index=True)
+
+
+class Payment(Base):
+    __tablename__ = "payments"
+
+    id = Column(Integer, primary_key=True, index=True)
+    amount = Column(Integer, index=True)
+    title = Column(String, index=True)
+    date = Column(Date, index=True)
+    transactionId = Column(Integer, index=True)
+    userId = Column(Integer, index=True)
+    paid = Column(Boolean, index=True)
+
+
+class Transaction(Base):
+    __tablename__ = "transactions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    userId = Column(Integer, index=True)
