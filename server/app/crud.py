2c6762746b0d4eb91338eb963ab4a470e7915108
Arzekeil-Abel De Leon <delearze@gmail.com>
2022-11-20 07:49:59 -0500

    Revert "Update apis so they work with patch and delete (#13)"
    This reverts commit a44d9e42674ff6bd634ce3767bbb69c734aa0e84.

--- server/app/crud.py
+++ server/app/crud.py
@@ -1,4 +1,5 @@
 from sqlalchemy.orm import Session
+
 from . import models
 
 
@@ -70,74 +71,39 @@ def create_user(db: Session, body):
     return user
 
 
-def update_payment(db: Session, id, body):
-    payment = db.query(models.Payment).filter(models.Payment.id == id).first()
-
-    if payment:
-        payment.transactionId = body.transactionId
-        payment.date = body.date
-        payment.paid = body.paid
-        payment.amount = body.amount
-        payment.title = body.title
-        payment.userId = body.userId
-
-        db.commit()
-        return db.query(models.Payment).filter(models.Payment.id == id).first()
-
-    return "no payment with id " + str(id)
+def update_payment(db: Session, body):
+    payment = models.User(id=body.id)
+    db.add(payment)
+    db.commit()
+    db.refresh(payment)
+    return payment
 
 
-def update_todo(db: Session, id, body):
-    todo = db.query(models.Todo).filter(models.Todo.id == id).first()
-
-    if todo:
-        todo.userId = body.userId
-        todo.isCompleted = body.isCompleted
-        todo.title = body.title
-        todo.dueDate = body.dueDate
-
-        db.commit()
-        return db.query(models.Todo).filter(models.Todo.id == id).first()
-
-    return "no task with id " + str(id)
+def update_todo(db: Session, body):
+    todo = models.Todo(id=body.id)
+    db.add(todo)
+    db.commit()
+    db.refresh(todo)
+    return todo
 
 
-def update_post(db: Session, id, body):
-    post = db.query(models.Post).filter(models.Post.id == id).first()
-
-    if post:
-        post.title = body.title
-        post.date = body.date
-        post.description = body.description
-        post.userId = body.userId
-
-        db.commit()
-        return db.query(models.Post).filter(models.Post.id == id).first()
-
-    return "no post with id " + str(id)
+def update_post(db: Session, body):
+    post = models.User(id=body.id)
+    db.add(post)
+    db.commit()
+    db.refresh(post)
+    return post
 
 
-def update_user(db: Session, id, body):
-    user = db.query(models.User).filter(models.User.id == id).first()
-
-    if user:
-        user.firstName = body.firstName
-        user.lastName = body.lastName
-        user.icon = body.icon
-        user.color = body.color
-
-        db.commit()
-        return db.query(models.User).filter(models.User.id == id).first()
-
-    return "no user with id " + str(id)
+def update_user(db: Session, body):
+    user = models.User(id=body.id)
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return user
 
 
-def delete_post(db: Session, id):
-    post = db.query(models.Post).filter(models.Post.id == id).first()
-
-    if post:
-        db.delete(post)
-        db.commit()
-        return "post with id " + str(id) + " deleted"
-    else:
-        return "no such post with id " + str(id)
+def delete_post(db: Session, body):
+    post = db.query().filter(models.Post.id == body.id).first()
+    db.delete(post)
+    db.commit()
